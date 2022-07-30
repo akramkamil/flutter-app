6398a301e94a0b841e647927026ca80e36edaa32
akram-kamil <akrambean@gmail.com>
2022-07-30 13:14:28 +0200

    makeing news list

---
+++ lib/models/news.dart
@@ -0,0 +1,8 @@
+class News {
+  String imgUrl;
+  String title;
+  String newsText;
+  bool isFavorite= false;
+
+  News ({required this.imgUrl, required this.title, required this.newsText});
+}
---
+++ lib/providers/news_provider.dart
@@ -0,0 +1,47 @@
+import 'package:news_application/models/news.dart';
+
+class Newprovider {
+  List <News> allNews= [
+    News(imgUrl: 'https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/gsfc_20220615_2022-148800-2_003.jpg?itok=ugr8Ef5v',
+        title: 'NASA Goddard Center Director Shares Plans to Retire',
+        newsText: '''Dennis Andrucyk, director of NASA’s Goddard Space Flight Center in Greenbelt, Maryland, has announced his intent to retire after more than 36 years of federal service, including multiple leadership roles at NASA and as Goddard’s director since January 2020.
+
+Andrucyk will continue to serve as Goddard center director while NASA conducts a nationwide search and open competition for a new director. Once a new director is selected, and following a short transition period, Andrucyk will retire; at this time, there is no set date for his retirement.
+
+“Dennis is an exceptional leader and a brilliant engineer who continues to serve NASA and our nation with honor and distinction,” said NASA Administrator Bill Nelson. “In my view, his crowning achievement at Goddard’s helm has been overseeing the brilliant launch and awe-inspiring first images release of the James Webb Space Telescope, a mission that represents our vision at NASA to explore the secrets of the universe for the benefit of all. I’m grateful for his decades of public service and continued dedication to Goddard, NASA, and our nation.”
+
+Among Goddard’s accomplishments during Andrucyk’s tenure to date are the successful launch and commissioning of the James Webb Space Telescope, which already is transforming our view of the universe. Other accomplishments during his tenure at Goddard include the Landsat 9 launch, OSIRIS-REx collecting a sample from the asteroid Bennu and beginning its return to Earth, Parker Solar Probe “touching” the Sun, championing NASA’s next Venus mission – DAVINCI – ongoing development of the Nancy Grace Roman Space Telescope, as well as the first Minotaur IV launch and multiple International Space Station cargo resupply missions from NASA’s Wallops Flight Facility on Wallpos Island, Virginia.
+
+In addition to several major mission milestones, Goddard also played a key role, under Dennis’ leadership, in celebrations marking the 50th anniversary of Earth Day, 75th anniversary of Wallops, and 30th anniversary of the Hubble Space Telescope. And all of this was done as Dennis led the center through the challenges of the COVID-19 pandemic.  
+
+Dennis joined the NASA family in 1988. Since then, he has been a recipient of the NASA Outstanding Leadership Medal, NASA Exceptional Service Medal, Goddard Outstanding Leadership Honor Award, and Goddard Exceptional Achievement Award in Diversity and Equal Employment Opportunity. Dennis is also a two-time recipient of the Senior Executive Service Meritorious Presidential Rank Award. 
+
+Learn more about Andrucyk’s career at NASA:'''),
+
+    News(imgUrl: 'https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/1-willamette-falls-1041.jpg?itok=dTE0cUfL',
+        title: 'US-European Satellite Will Make World’s First Global Freshwater Survey',
+        newsText: '''Water is life, but for all its importance, humanity has a surprisingly limited view of Earth’s freshwater bodies. Researchers have reliable water level measurements for only a few thousand lakes around the world, and little to no data on some of the planet’s important river systems. The upcoming Surface Water and Ocean Topography (SWOT) satellite will fill that enormous gap. By helping to provide a better understanding of Earth’s water cycle, it will both aid in better management of water resources and expand knowledge of how climate change affects lakes, rivers, and reservoirs.
+
+A collaboration between NASA and the French space agency Centre National d’Études Spatial (CNES), with contributions from the Canadian Space Agency and the United Kingdom Space Agency, SWOT is scheduled to launch in November from Vandenberg Space Force Base in California. Engineers and technicians are finishing up work on the satellite in a facility run by Thales Alenia Space in Cannes, France.
+
+SWOT has several key tasks, including measuring the height of water bodies on Earth’s surface. Over the ocean, the satellite will be able to “see” features like eddies less than 60 miles (100 kilometers) across – smaller than those that previous sea level satellites could observe. SWOT will also measure more than 95% of Earth’s lakes larger than 15 acres (6 hectares) and rivers wider than 330 feet (100 meters) across.
+
+“Current databases maybe have information on a couple thousand lakes around the world,” said Tamlin Pavelsky, the NASA freshwater science lead for SWOT, based at the University of North Carolina, Chapel Hill. “SWOT will push that number to between 2 million and 6 million.”
+
+Along with measuring the water height – whether it be in a lake, river, or reservoir – SWOT will also measure its extent, or surface area. That crucial information will enable scientists to calculate how much water moves through freshwater bodies. “Once you get hold of the volume of water, you can better assess the water budget, or how much water flows into and out of an area,” said Lee-Lueng Fu, SWOT project scientist at NASA’s Jet Propulsion Laboratory in Southern California, which manages the U.S. portion of the mission.
+
+This is important because climate change is accelerating Earth’s water cycle. Warmer temperatures mean the atmosphere can hold more water (in the form of water vapor), which can cause, for example, rain storms to be stronger than a region might typically see. This, in turn, can wreak havoc on farms, damaging crops. Such accelerating changes can make managing a community’s water resources more difficult.  
+
+“As Earth’s water cycle intensifies, predicting future extreme events like floods and droughts requires monitoring both changes in water supply from the ocean and water demand and usage on land. SWOT’s global look at all surface water on Earth will give us exactly that,” said Nadya Vinogradova Shiffer, SWOT’s program scientist at NASA Headquarters in Washington.'''),
+
+    News(imgUrl: 'https://www.nasa.gov/sites/default/files/styles/ubernode_alt_horiz/public/thumbnails/image/landsat9_still_sw2_print_0.jpeg',
+        title: 'Landsat Legacy: NASA-USGS Program Observing Earth from Space Turns 50',
+        newsText: '''On a warm July day in 1972, NASA launched a new Earth-imaging satellite called the Earth Resources Technology Satellite. “ERTS” was the first satellite of what later became NASA and the U.S. Geological Survey’s Landsat Program, an ambitious effort with a vision of documenting the entirety of Earth from space. The first Landsat was so successful it led to a series of satellites that have created the longest contiguous record of Earth’s surface from a space-eye view – that continues growing to this day, 50 years later.
+
+“The early Landsats revolutionized the way we observed the Earth from space,” said Jim Irons, director emeritus of the Earth Sciences Division at NASA’s Goddard Space Flight Center in Greenbelt, Maryland.
+
+Since its debut, Landsat has amassed over 10 million images. These images, also called scenes, show current snapshots of land and coasts, but pair with images of years past and they also reveal changes through time – glaciers slowly disappearing, or urban spaces sprawling across the landscape.
+
+These scenes and time series have myriad applications around the globe: Hydrologists use them to track how rivers change; ecologists use them to determine the extent of deforestation; farmers and agricultural organizations use them to analyze crop health.''')
+  ]
+}
